f1b3714838dc137b25e4cbbc2977566c059f086d
Liam <liampassion6626@gmail.com>
2017-10-30 22:45:49 +0800

    updating Class with exercise_09.cc

--- Class/example_09.cc
+++ Class/example_09.cc
@@ -1,4 +1,4 @@
-//File:Struct example code 07 
+//File:Struct example code 08 
 //Purpose: struct with Function parameter called by reference  
 //Date:16/10/17 
 //Author:Tzu-Lu Lin
@@ -13,19 +13,19 @@ struct Time
     int hour;
     int minute;
     int second;
+
 };
 
-void printTime (Time &t)
+void printTime (const Time &t)
 {
-    cout<<t.hour   <<":";
-    cout<<t.minute <<":";
-    cout<<t.second <<endl;
+    cout<<t.hour    <<":";
+    cout<<t.minute  <<":";
+    cout<<t.second  <<endl;
 }
 
 int main()
 {
     Time breakfast = {6,30,0};
-    printTime = (breakfast);
+    printTime (breakfast);
     return 0;
-
 }
