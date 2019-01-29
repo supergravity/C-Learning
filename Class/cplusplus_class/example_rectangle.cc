5c96c94eb41fecf6e9363cb8e996f3f32fe497f6
Liam <liampassion6626@gmail.com>
2019-01-29 22:25:40 +0800

    add documents

---
+++ Class/cplusplus_class/example_rectangle.cc
@@ -0,0 +1,19 @@
+
+
+
+#include <iostream>
+#include <stdio.h>
+
+class Rectangle 
+{
+    double width   = -999;
+    double height  = -999;
+
+    public:
+    void set_value (int, int);
+    double
+
+
+
+}
+
