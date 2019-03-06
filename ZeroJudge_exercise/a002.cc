3bef661c409b07d2c718797185ac441905995ede
Liam <liampassion6626@gmail.com>
2019-03-06 14:52:16 +0800

    adding a002.cc

---
+++ ZeroJudge_exercise/a002.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+using namespace std;
+int main()
+{
+    int a;
+    int b;
+    while(cin>>a>>b)
+    {
+        cout<<a+b<<endl; 
+    }
+}
+
+//#include <iostream>
+//#include <stdio.h>
+//#include <cmath.h>
+//
+//int main()
+//{
+//    bool t = 1;
+//    while(t)
+//    {
+//        //initialization
+//        int num_1 = 0;
+//        int num_2 = 0;
+//        int sum   = 0;
+//        //std::cout<<"Please insert two integer number:"<<std::endl; 
+//        std::cin>>num_1>>std::endl;
+//        std::cin>>num_2>>std::endl;
+//        if ((fabs(num_1) > 10*pow(6)) || (fabs(num_2) > 10*pow(6)))
+//        {
+//            break;
+//        }
+//        
+//        sum = num_1 + num_2;
+//        
+//        std::cout<<num_1<<'\t';
+//        std::cout<<num_2<<std::endl;
+//        std::cout<<sum<<std::endl;
+//    }
+//
+//}
