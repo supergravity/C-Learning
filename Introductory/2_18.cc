f48c3f02215821d221fb850fb3fab13c9ddd8088
Liam <liampassion6626@gmail.com>
2019-09-19 17:42:08 +0800

    add files

---
+++ Introductory/2_18.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+
+
+int main()
+{
+    std::cout<<"Inventory"<<std::endl;
+    std::cout<<"Date:"<<std::endl;
+    std::cout<<"NO.	Name	Purchase	Shipment	In Stock"<<std::endl;
+    std::cout<<"--------------------------------------------------------"<<std::endl;
+    std::cout<<"--------------------------------------------------------"<<std::endl;
+
+    return 0;
+}
---
+++ Introductory/2_19.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+
+
+int main()
+{
+    std::cout<<"Grades"<<std::endl;
+    std::cout<<"Date:"<<std::endl;
+    std::cout<<"Name	Chinese    English    Mathematics    Total"<<std::endl;
+    std::cout<<"--------------------------------------------------------"<<std::endl;
+    std::cout<<"--------------------------------------------------------"<<std::endl;
+    std::cout<<"Total Score Average:"<<std::endl;
+    std::cout<<"First Quarter Score Average:"<<std::endl;
+    std::cout<<"Half Score Average:"<<std::endl;
+
+    return 0;
+}
